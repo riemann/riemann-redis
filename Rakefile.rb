938054a50705ad4c0d54fb627b003cce9159d0d5
Federico Borgnia <fborgnia@gmail.com>
2014-03-28 17:03:51 -0500

    rakefile for gem generatin

--- Rakefile.rb
+++ Rakefile.rb
@@ -9,31 +9,23 @@ ENV['COPYFILE_DISABLE'] = 'true'
 
 # Gemspec
 gemspec = Gem::Specification.new do |s|
-  s.rubyforge_project = 'riemann-tools'
+  s.rubyforge_project = 'riemann-redis'
 
-  s.name = 'riemann-tools'
-  s.version = '0.2.0'
-  s.author = 'Kyle Kingsbury'
-  s.email = 'aphyr@aphyr.com'
-  s.homepage = 'https://github.com/aphyr/riemann-tools'
+  s.name = 'riemann-redis'
+  s.version = '0.1.0'
+  s.author = 'Fede Borgnia'
+  s.email = 'fborgnia@gmail.com'
+  s.homepage = 'https://github.com/riemann/riemann-redis'
   s.platform = Gem::Platform::RUBY
-  s.summary = 'Utilities which submit events to Riemann.'
-  s.description = 'Utilities which submit events to Riemann.'
-  s.license = 'MIT'
+  s.summary = 'Redis client that submits events to Riemann.'
 
-  s.add_dependency 'riemann-client', '>= 0.2.2'
-  s.add_dependency 'trollop', '>= 1.16.2'
-  s.add_dependency 'munin-ruby', '>= 0.2.1'
-  s.add_dependency 'yajl-ruby', '>= 1.1.0'
+  s.add_dependency 'riemann-tools', '>= 0.2.1'
   s.add_dependency 'redis', '>= 3.0.2'
-  s.add_dependency 'fog', '>= 1.4.0'
-  s.add_dependency 'faraday', '>= 0.8.5'
-  s.add_dependency 'nokogiri', '>= 1.5.6'
 
-  s.files = FileList['lib/**/*', 'bin/*', 'LICENSE', 'README.markdown'].to_a
+  s.files = FileList['lib/**/*', 'bin/*', 'LICENSE', 'README.md'].to_a
   s.executables |= Dir.entries('bin/')
   s.require_path = 'lib'
-  s.has_rdoc = true
+  s.has_rdoc = false
 
   s.required_ruby_version = '>= 1.8.7'
 end
@@ -42,8 +34,8 @@ Gem::PackageTask.new gemspec do |p|
 end
 
 RDoc::Task.new do |rd|
-  rd.main = 'Riemann Tools'
-  rd.title = 'Riemann Tools'
+  rd.main = 'Riemann redis'
+  rd.title = 'Riemann redis'
   rd.rdoc_dir = 'doc'
 
   rd.rdoc_files.include('lib/**/*.rb')
