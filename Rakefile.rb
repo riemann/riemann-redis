25221df658a4132f4889a58978222ca869af7e64
James Turnbull <james@lovedthanlost.net>
2019-01-14 02:52:14 -0500

    Updated versions and added license

--- Rakefile.rb
+++ Rakefile.rb
@@ -14,12 +14,13 @@ gemspec = Gem::Specification.new do |s|
   s.name = 'riemann-redis'
   s.version = '0.2.1'
   s.author = 'Fede Borgnia'
+  s.license = 'MIT'
   s.email = 'fborgnia@gmail.com'
   s.homepage = 'https://github.com/riemann/riemann-redis'
   s.platform = Gem::Platform::RUBY
   s.summary = 'Redis client that submits events to Riemann.'
 
-  s.add_dependency 'riemann-tools', '>= 0.2.1'
+  s.add_dependency 'riemann-tools', '~> 0.2.18'
   s.add_dependency 'redis', '>= 3.0.2'
 
   s.files = FileList['lib/**/*', 'bin/*', 'LICENSE', 'README.md'].to_a
